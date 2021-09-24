fd62591504a7c6d0151308a405f941e92e01c289
Puvan Kumar V <64190547+puvankumarv@users.noreply.github.com>
2021-09-24 19:07:07 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$telephone= $_POST['telephone'];
+$sub= $_POST['subject'];
+$message= $_POST['message'];
+
+$to = "puvanvenkatraman@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n telephone=" . $telephone . "\r\n Subject = " .$sub . "\r\n Message=" . $message  ;
+$headers = "From: puvankumarv@gmail.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
